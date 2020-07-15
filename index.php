f24e7e2628cc2a93174291c9ab92f01c0574163b
hendrasolih <hendra.solih.jp@gmail.com>
2020-07-15 22:20:36 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, reiciendis tempora ducimus, dolore nesciunt quod, itaque doloribus doloremque totam suscipit temporibus et facilis soluta rem assumenda inventore at molestiae sint.</p>
 </body>
 </html>
