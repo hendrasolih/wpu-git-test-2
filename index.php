e034d8f5206ee07144745e6148f49a9dadf37139
hendrasolih <hendra.solih.jp@gmail.com>
2020-07-15 22:26:24 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, reiciendis tempora ducimus, dolore nesciunt quod, itaque doloribus doloremque totam suscipit temporibus et facilis soluta rem assumenda inventore at molestiae sint.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
